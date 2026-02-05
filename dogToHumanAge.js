4ea6e9c28581123e426487ad8d56400013985e62
shred-der <teminimighty@gmail.com>
2026-02-05 03:16:41 +0100

    x++ (increment after) ++x (increment first)

---
+++ dogToHumanAge.js
@@ -0,0 +1,8 @@
+// Create a dog age to human age converter using the formula below
+// humanAge = ((dogAge - 2) * 4) + 21;
+// Your code is going to create a prompt that askes the users for their dog's age and then gives the answert back through an alert
+
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is " + humanAge + " years old in Human years.")
+console.log(("Your dog is " + humanAge + " years old in Human years."))
