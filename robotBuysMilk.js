1811eb1ee2daf48d1c700ee3392a28ba97baebd4
shred-der <teminimighty@gmail.com>
2026-02-05 05:01:24 +0100

    sleepy af

---
+++ robotBuysMilk.js
@@ -0,0 +1,47 @@
+// Edit the function below that sends the robot on an errand to buy milk from the store
+// The function should take money as a parameter and indicate the number of bottles the money is able to buy
+
+function getMilk0 (bottles) {
+    console.log("LeaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " + bottles + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+function getMilk1 (money) {
+    var bottles = Math.trunc(money / 1.5);
+    console.log("LeaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " + bottles + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
