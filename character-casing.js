a0a3e4e7679f3f99eb4e691f3bc341d609b6a657
shred-der <teminimighty@gmail.com>
2026-02-05 02:44:25 +0100

    I understand the colors on the screen now

--- character-casing.js
+++ character-casing.js
@@ -1,3 +1,8 @@
 // Create a prompt that asks for a name and then formats the name to give the format below irrespective of the alphabetical casing that was inputed using only what you've learned so far
 // "Hello Angela"
 
+var name = prompt("What's your name?");
+var formattedName = (name.slice(0,1)).toUpperCase() + (name.slice(1,name.length)).toLowerCase();
+alert("Hello " + formattedName);
+console.log("Hello " + formattedName)
+
